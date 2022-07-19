d8b604f3ef81bae44089bc735bf0679be100da1b
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-07-18 22:49:29 -0300

    cocktail finalizado

--- projects/cocktail-project/script.js
+++ projects/cocktail-project/script.js
@@ -1,5 +1,7 @@
 const input = document.querySelector('.input')
 const button = document.querySelector('.button')
+const home = document.querySelector('.home')
+const returnDrink = document.querySelector('.return')
 
 function deleteDrinks() {
     const drink = document.getElementsByClassName('drink')
@@ -12,7 +14,7 @@ function deleteDrinks() {
   }
 }
 
-const fetchDrinks = async (ingredient) => {
+const fetchIngredient = async (ingredient) => {
     try {
       if (ingredient === undefined) {
         throw new Error('You must provide an url');
@@ -26,7 +28,7 @@ const fetchDrinks = async (ingredient) => {
     }
   };
 
-const fetchRecipe = async (drink) => {
+  const fetchRecipe = async (drink) => {
       const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
       const request = await fetch(url);
       const json = await request.json();
@@ -86,16 +88,51 @@ const fetchRecipe = async (drink) => {
    
   async function drinkList() {
     deleteDrinks();
-    const data = await fetchDrinks(input.value.replace(' ','+'));
+    const data = await fetchIngredient(input.value.replace(' ','+'));
     data.drinks.forEach(({ strDrink, strDrinkThumb }) => {
         const section = document.createElement('section');
         section.className = 'drink';
         section.addEventListener('click', recipe)
         section.appendChild(createCustomElement('span', 'drink__title', strDrink));
         section.appendChild(createProductImageElement(strDrinkThumb));
+        section.appendChild(createCustomElement('button', 'drink__button', 'Recipe'));
         document.querySelector('.drinks').appendChild(section);
     });
   }
 
+  async function drinkList() {
+    if (input.value !== '' ) {
+    deleteDrinks();
+    const data = await fetchIngredient(input.value.replace(' ','+'));
+    alldrinks(data) }
+    else {
+      drinks();
+    }
+  }
+
+  async function drinks() {
+    input.value = ''
+    deleteDrinks();
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+    const request = await fetch(url)
+    const json = await request.json()
+    alldrinks(json)}
+
+  function alldrinks(data) {
+    if (data !== undefined) {
+    data.drinks.forEach(({ strDrink, strDrinkThumb }) => {
+        const section = document.createElement('section');
+        section.className = 'drink';
+        section.addEventListener('click', recipe)
+        section.appendChild(createCustomElement('span', 'drink__title', strDrink));
+        section.appendChild(createProductImageElement(strDrinkThumb));
+        section.appendChild(createCustomElement('button', 'drink__button', 'Recipe'));
+        document.querySelector('.drinks').appendChild(section);
+    });
+  }}
 
 button.addEventListener('click', drinkList)
+home.addEventListener('click', drinks)
+returnDrink.addEventListener('click', drinkList)
+
+window.onload = () => drinks()
